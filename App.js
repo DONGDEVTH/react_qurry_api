eca843f4ce7dbb10b01fd52480def1d301e1a9bf
DONGDEVTH <51239005+DONGDEVTH@users.noreply.github.com>
2023-02-20 11:54:45 +0700

    React_qurry_API

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Awnsers from './components/Awnsers';
+//import First from './components/First';
+import Quizes from './components/Quizes';
+
+
+
+
+export default function App() {
+    return (
+
+        <Quizes />
+        
+    )
+}
